b1cd43a9455ab11af0d7ccaef2e0cb6fde004c57
ahmedehab311 <104663084+ahmedehab311@users.noreply.github.com>
2024-06-09 00:37:13 +0300

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,74 +1,73 @@
-import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Main() {
-  const [countries, setCountries] = useState([]);
-  const [selectedCountry, setSelectedCountry] = useState("");
+  const [countrties, setCountries] = useState([]);
   const [prayerTimings, setPrayerTimings] = useState({});
- 
+  const [selectCountry, setSelectCountry] = useState("");
+
   useEffect(() => {
-    const fetchCountries = async () => {
+    const fetchCountry = async () => {
       try {
-        const response = await axios.get(
-          `https://api.aladhan.com/v1/methods`
-        );
-        const countryNames = Object.keys(response.data.data);
-        setCountries(countryNames);
-        console.log(response.data.data)
+        const response = await axios.get(`https://api.aladhan.com/v1/methods`);
+        const countryname = Object.keys(response.data.data);
+        setCountries(countryname);
+        console.log(response.data.data);
       } catch (error) {
-        console.error("Error fetching countries:", error);
+        console.error("error fetching country", error);
       }
     };
-  
-    fetchCountries();
+    fetchCountry();
   }, []);
-  
-  const handleCountryChange = async (selectedCountry) => {
+
+  const handleCountryChange = async (selectCountry) => {
     try {
-      const response = await axios.get(
-        `https://api.aladhan.com/v1/methods`
-      );
-      const prayerData = response.data.data[selectedCountry];
+      const response = await axios.get(`https://api.aladhan.com/v1/methods`);
+
+      const prayerData = response.data.data[selectCountry];
       setPrayerTimings(prayerData);
     } catch (error) {
-      console.error("Error fetching prayer timings:", error);
+      console.error("error fatching prayer time", error);
     }
   };
+
   return (
-    <div className="container">
-      <h2>Select a country:</h2>
+    <>
+     <div className="container">
+     <h2>:Select a country</h2>
       <select
-        className="select-country"
-        value={selectedCountry}
+      className="select-country"
+        value={selectCountry}
         onChange={(e) => {
-          setSelectedCountry(e.target.value);
+          setSelectCountry(e.target.value);
           handleCountryChange(e.target.value);
         }}
       >
-        <option value="">Select a country</option>
-        {countries.map((country, index) => (
-          <option key={index} value={country}>
-            {country}
+        <option value="">select a country</option>
+        {countrties.map((item, index) => (
+          <option   value={item} key={index}>
+            {item}
           </option>
         ))}
       </select>
-  
+
       {prayerTimings && Object.keys(prayerTimings).length > 0 && (
         <div className="prayer-timings">
-          <h2>Prayer Timings for:  {selectedCountry}</h2>
+          <h2>prayer timing for {selectCountry}</h2>
           <div>
             {prayerTimings.params && prayerTimings.params.Fajr && (
-              <span>Fajr : {prayerTimings.params.Fajr}</span>
+              <span>fajr{prayerTimings.params.Fajr} </span>
             )}
           </div>
           <div>
             {prayerTimings.params && prayerTimings.params.Isha && (
-              <span>Isha : {prayerTimings.params.Isha}</span>
+              <span>fajr{prayerTimings.params.Isha} </span>
             )}
           </div>
         </div>
       )}
-    </div>
+     </div>
+    </>
   );
 }
 
